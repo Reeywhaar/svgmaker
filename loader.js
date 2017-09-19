2561b73dbc0c03f2e67234b8f6e1f4899255fc6c
Mikhail Vyrtsev <reeywhaar@gmail.com>
2017-09-20 02:43:05 +0300

    add webpack loader

---
+++ loader.js
@@ -0,0 +1,19 @@
+// To use it must be configured in webpack, because it doesn't ships as separate package
+// add to webpack config.js
+// resolveLoader: {
+// 	alias: {
+// 		'svg-js-loader': path.join(__dirname, 'node_modules/@reeywhaar/svgmaker', 'loader.js')
+// 	}
+// },
+
+const svgmaker = require(".index.js");
+
+module.exports = function SVGLoader(source) {
+	if(this.cacheable) this.cacheable();
+	if(this.resourcePath in require.cache){
+		delete require.cache[this.resourcePath];
+	}
+	const svg = require(this.resourcePath).toString() + "\n";
+	this.value = svg;
+	return `module.exports = \`${svg}\``;
+}
